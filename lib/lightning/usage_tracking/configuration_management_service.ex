73867c1552ee7f0da2f42fb5dabf2769c15191b5
Rory McKinley <rorymckinley@gmail.com>
2024-03-08 07:24:46 +0200

    Fix

--- lib/lightning/usage_tracking/configuration_management_service.ex
+++ lib/lightning/usage_tracking/configuration_management_service.ex
@@ -31,7 +31,7 @@ defmodule Lightning.UsageTracking.ConfigurationManagementService do
     end
   end
 
-  def create_enabled_config(enabled_at, start_reporting_after) do
+  defp create_enabled_config(enabled_at, start_reporting_after) do
     %DailyReportConfiguration{
       tracking_enabled_at: enabled_at,
       start_reporting_after: start_reporting_after
@@ -39,7 +39,7 @@ defmodule Lightning.UsageTracking.ConfigurationManagementService do
     |> Repo.insert!()
   end
 
-  def enable_config(config, enabled_at, start_reporting_after) do
+  defp enable_config(config, enabled_at, start_reporting_after) do
     config
     |> Changeset.change(
       tracking_enabled_at: enabled_at,
